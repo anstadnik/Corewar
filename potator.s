9d5fa0231d8b5fce3030e6ab9d6a789d7d024b0d
Stadnik Andrii <astadnik@student.unit.ua>
2018-07-08 13:38:42 +0300

    New bot

--- potator.s
+++ potator.s
@@ -1,39 +1,42 @@
 .name "Potator"
 .comment "The beater"
 
-	sti	r1, %:live, %1 # Load player's number to live
-:load
-	# Change those loop labels to something normal
-	ld :loop, r1
-	ld :loop + 1, r2
-	ld :loop + 2, r3
-	ld :loop + 3, r4
-	ld :loop + 4, r5
-	ld :loop + 5, r6
-	ld :loop + 6, r7
-	ld :loop + 7, r8
-	ld :loop + 8, r9
-	ld :loop + 9, r10
-	ld :loop + 10, r11
-	ld :loop + 11, r12
-	ld %1, r13
-	ld %0, r15
-	ld %12, r16
+beg:
+    sti r1, %:live,   %1
+    sti r1, %:hehehe, %2
+    ld      :hehehe,  r2
+    ld      %0,       r6        # Change carry to 1
 
-:loop
-	add r15, r13, r15 # Increment counter
-	add r16, r13, r16 # Increment counter
-	sti r15, %:quine, %1 # Write parameters
-	sti r16, %:quine, %2 # Write parameters
-:quine
-	st r42, 42 # Write code from register to map
-	xor r16, %12, r14 # Check if u copied all the code
-	zjmp %:next_iteration
-	zjmp %:loop
-:next_iteration
-	ld %0 r15
-	ld %12 r16
 :live
-	live %42
-	fork %13
-	zjmp %:loop
+    live    %42
+    fork    %prep_f
+    fork    %prep_b
+    zjmp    :live
+
+
+:prep_b
+    ld      %-8,      r4
+
+:attak_b
+    st      r2,       r4        # Write smth to a field
+    sub     r4,       r5,    r4 # Decrement a counter
+    xor     r3,       %-256, r6
+    zjmp    :prep_b
+    ld      %0,       r6        # Change carry to 1
+    zjmp    :attak_b
+
+
+:prep_f
+    ld      %1,       r3
+
+:attak_f
+    st      r2,       r3        # Write smth to a field
+    add     r3,       r5,    r3 # Increment a counter
+    xor     r3,       %256,  r6
+    zjmp    :prep_f
+    ld      %0,       r6        # Change carry to 1
+    zjmp    :attak_f
+
+                                # We will spam this
+hehehe:
+    ld      %43,      r1
